6ee31023154a44e171a89828508e367212a8d411
sedwarb <sedwarb@gmail.com>
2022-04-18 14:13:42 -0500

    put charge pokemons since this

--- client/src/components/landing/landing.jsx
+++ client/src/components/landing/landing.jsx
@@ -1,7 +1,13 @@
-import React from 'react';
+import React,{useEffect} from 'react';
+import { useDispatch } from 'react-redux'
 import { NavLink } from 'react-router-dom';
+import {getPokemons} from '../../actions/actions'
 
-export default function landing() {
+export default function Landing() {
+    const dispatch = useDispatch()
+    useEffect(()=>{
+        dispatch(getPokemons())    
+      },[])//se ejecuta una sola vez
     return (
         <div className="landing" > 
             <img  src="https://fontmeme.com/images/Pokemon-Logo.jpg" alt='Pokemon' />

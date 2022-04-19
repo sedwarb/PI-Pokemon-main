403ef300680e79c39f816f7a2760b748f380e435
sedwarb <sedwarb@gmail.com>
2022-04-19 00:52:42 -0500

    added route to create pokemon

--- client/src/App.js
+++ client/src/App.js
@@ -5,14 +5,15 @@ import Landing from "./components/landing/Landing"
 import { Route } from "react-router-dom";
 import {Busqueda} from './components/Busqueda/Busqueda';
 import {Detail} from './components/detail/detail'
+import { CrearPokemon } from './components/CrearPokemon/CrearPokemon';
 
 function App() {
-  //<NavBar />
   return (
     <React.Fragment>      
       <Route exact path="/" component={Landing}></Route>
       <Route path="/:x" component={NavBar}></Route>
       <Route exact path="/pokemons" component={Busqueda}></Route>
+      <Route exact path="/pokemons/create" component={CrearPokemon}></Route>
       <Route exact path="/pokemons/detail" component={Detail}></Route>
     </React.Fragment>
   );

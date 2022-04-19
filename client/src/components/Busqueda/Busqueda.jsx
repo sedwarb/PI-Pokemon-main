67fa601642fb70929e7e7faab4b3fcf5c0f6c9b1
sedwarb <sedwarb@gmail.com>
2022-04-19 17:41:19 -0500

    add functions to move to the end / begin and update lines of code

--- client/src/components/Busqueda/Busqueda.jsx
+++ client/src/components/Busqueda/Busqueda.jsx
@@ -1,42 +1,46 @@
-import React,{useEffect,useState} from 'react'
+import React,{useState} from 'react'
 import { useSelector,useDispatch } from 'react-redux'
-import {getPokemons,orderA,orderD,orderAsc,orderDes} from '../../actions/actions'
+import {reset,orderA,orderD,orderAsc,orderDes,orderByType} from '../../actions/actions'
 import './Busqueda.css'
 
 export function Busqueda() {
   const [pagina, setPagina] = useState(0);
+  const [pokeName, setPokeName] = useState({});
   const dispatch = useDispatch()
   const pokemons = useSelector(state=>state.pokemonsLoaded)
   const asc = useSelector(state=>state.oAsc)
   const des = useSelector(state=>state.oDes)
   const orden = useSelector(state=>state.orden)
-  /*
-  useEffect(()=>{
-    dispatch(getPokemons())    
-  },[])//se ejecuta una sola vez
-  */
+  
   function oAsc(){    
     if(asc===false){
-      dispatch(orderA(pokemons))
+      dispatch(orderA(orden))
       dispatch(orderAsc(true))
       dispatch(orderDes(false))
     }
   }
-
   function oDec(){
     if(des===false){
-      dispatch(orderD(pokemons))
+      dispatch(orderD(orden))
       dispatch(orderDes(true))
       dispatch(orderAsc(false))
     }
   }
-
+  function ultimo(){
+    if(orden.length/12<=1)setPagina(0)
+    if(orden.length/12>1 && orden.length/12<=2)setPagina(12)
+    if(orden.length/12>2 && orden.length/12<=3)setPagina(24)
+    if(orden.length/12>3 && orden.length/12<=4)setPagina(36)
+  }
   function paginasig(){
-    if(pagina<36)setPagina(pagina+12)
+    if(pagina<36 && orden.length>12)setPagina(pagina+12)
   }
   function paginaant(){
     if(pagina>=12)setPagina(pagina-12)
   }
+  function handleChange(e){
+    setPokeName({[e.target.id]: e.target.value})
+  }  
   //<div key={`pokef${i}`}>{p.estadisticas.fuerza}</div>
   //<div key={`2pokef${i}`}>{p.estadisticas.fuerza}</div>
   //<div key={`3pokef${i}`}>{p.estadisticas.fuerza}</div>
@@ -44,20 +48,18 @@ export function Busqueda() {
   return (
     <div key={`lista${pagina}`} className='columna'>
       <div key='boton' className='boton'>
-        <form onSubmit={(e) => {
-          e.preventDefault();
-          paginasig();
-        }}>
-          <input key='sb' type="submit" value={`Pag Sig: ${pagina}`} />
-        </form>
-        <form onSubmit={(e) => {
-          e.preventDefault();
-          paginaant();
-        }}>
-          <input key='ab' type="submit" value={`Pag Ant...`} />          
-        </form>
-        <input key='db' type="button" onClick={oDec} value="Decr."/>
+        <input key='db0' type="button" onClick={()=>setPagina(0)} value="|<<"/>
+        <input key='sb' type="button" onClick={()=>paginaant()} value="|<"/>
+        <input key='db' type="button" onClick={oDec} value="Decr."/>        
         <input key='Ab' type="button" onClick={oAsc} value="Asce."/>
+        <input key='ab' type="button" onClick={()=>paginasig()} value=">|"/>
+        <input key='Ab' type="button" onClick={ultimo} value=">>|"/>
+        <input key='Re' type="button" onClick={()=>dispatch(reset(pokemons))} value="Reset."/>
+      </div>
+      <div>
+        <span className='tiSpan'>ByType...     </span>
+        <input type='search' id="tipo" onChange={(e) => handleChange(e)} />
+        <input type="button" onClick={()=>dispatch(orderByType(pokemons,pokeName.tipo))} value="Buscar" />
       </div>
       <div key={`uno${pokemons.lenght}`} className='firstR'>
         {
@@ -67,8 +69,7 @@ export function Busqueda() {
                 <div key={`poke${i}`}>
                   <img className='ipoke' src={p.imagen} alt='Pokemon' key={`imgpoke${i}`}/>
                   <div key={`pokeh1${i}`}>{p.nombre}</div>                  
-                  <div key={`poke2${i}`}>{p.tipos.map((t,j)=><div key={`pokeh2${i}${j}`}>{t}</div>)}</div>
-                  
+                  <div key={`poke2${i}`}>{p.tipos.map((t,j)=><div key={`pokeh2${i}${j}`}>{t}</div>)}</div>                  
                 </div>
               )
             }        
@@ -83,8 +84,7 @@ export function Busqueda() {
                 <div key={`2poke${i}`}>
                   <img className='ipoke' src={p.imagen} alt='Pokemon' key={`2imgpoke${i}`}/>
                   <div key={`2pokeh1${i}`}>{p.nombre}</div>
-                  <div key={`2poke2${i}`}>{p.tipos.map((t,j)=><div key={`2pokeh2${i}${j}`}>{t}</div>)}</div>
-                  
+                  <div key={`2poke2${i}`}>{p.tipos.map((t,j)=><div key={`2pokeh2${i}${j}`}>{t}</div>)}</div>                  
                 </div>
               )
             }        
@@ -99,8 +99,7 @@ export function Busqueda() {
                 <div key={`3poke${i}`}>
                   <img className='ipoke' src={p.imagen} alt='Pokemon' key={`2imgpoke${i}`}/>
                   <div key={`3pokeh1${i}`}>{p.nombre}</div>
-                  <div key={`3poke2${i}`}>{p.tipos.map((t,j)=><div key={`2pokeh2${i}${j}`}>{t}</div>)}</div>
-                  
+                  <div key={`3poke2${i}`}>{p.tipos.map((t,j)=><div key={`2pokeh2${i}${j}`}>{t}</div>)}</div>                  
                 </div>
               )
             }        

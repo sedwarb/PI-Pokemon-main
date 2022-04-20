3c5154e0a35ccdfbbbb7128b38642c0d53f17fb8
sedwarb <sedwarb@gmail.com>
2022-04-20 00:22:17 -0500

    Add types list

--- client/src/components/CrearPokemon/CrearPokemon.jsx
+++ client/src/components/CrearPokemon/CrearPokemon.jsx
@@ -1,20 +1,31 @@
 import React,{useState} from 'react'
-import { useDispatch } from 'react-redux'
+import { useDispatch,useSelector } from 'react-redux'
 import {createPokemons} from "../../actions/actions"
-
+import './CrearPokemon.css'
 
 export function CrearPokemon(){
-  const [datos, setPagina] = useState({});
+  const [datos, setPagina] = useState({acum:true});
+  const [sTipo, setSTipo] = useState([]);
   const dispatch = useDispatch()
+  const tipos = useSelector(state=>state.pokemonTypes)
   function handleChange(e){
     setPagina({...datos,[e.target.id]:e.target.value})
   }
+  function ftipo(e){
+    if(datos.acum){
+      setPagina({acum: false})
+    }else{
+      setSTipo([...sTipo,e.target.value])
+      setPagina({type:sTipo})
+      setPagina({acum: true})
+    }
+  }
   function creaObj(){
-    let tipo=[datos.tipo]
+    //let tipo=[datos.tipo]
     return {
       imagen:datos.imagen,
       nombre:datos.nombre,
-      tipo:tipo,
+      tipo:datos.type,
       altura:datos.altura,
       peso:datos.peso,
       vida:datos.vida,
@@ -27,8 +38,8 @@ export function CrearPokemon(){
   <div>
     <form onSubmit={(e) => {
         e.preventDefault();
-        //console.log(JSON.stringify(creaObj()))
-        dispatch(createPokemons(creaObj()))
+        //dispatch(createPokemons(creaObj()))
+        console.log(JSON.stringify(creaObj()))
       }}>
       <div>Datos del Pokemon</div>
       <br />
@@ -37,7 +48,14 @@ export function CrearPokemon(){
       <div>Nombre</div>
       <div><input type="text" id="nombre" onChange={(e) => handleChange(e)} /></div>
       <div>Tipo</div>
-      <div><input type="text" id="tipo" onChange={(e) => handleChange(e)} /></div>
+      <select name="tipos" id="tipos" onClick={(e)=>ftipo(e)}>
+          {tipos.map((tipo,i)=>{
+            return <option key={`plista${i}`} value={tipo} id={i+1} >{tipo}</option>
+          })}
+      </select>
+      <div className='divTipo' >{sTipo.map(p=>{
+          return <div>{`-${p}-`}</div>
+      })}</div>
       <div>Altura</div>
       <div><input type="text" id="altura" onChange={(e) => handleChange(e)} /></div>
       <div>Peso</div>

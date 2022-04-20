7181bb0c867d96440b48037c0042b2514094631c
sedwarb <sedwarb@gmail.com>
2022-04-20 00:18:11 -0500

    add charge types of pokemons

--- client/src/components/landing/landing.jsx
+++ client/src/components/landing/landing.jsx
@@ -1,13 +1,14 @@
 import React,{useEffect} from 'react';
 import { useDispatch } from 'react-redux'
 import { NavLink } from 'react-router-dom';
-import {getPokemons} from '../../actions/actions'
+import {getPokemons,tiposPokemon} from '../../actions/actions'
 
 export default function Landing() {
     const dispatch = useDispatch()
     useEffect(()=>{
-        dispatch(getPokemons())    
-      },[])//se ejecuta una sola vez
+        dispatch(getPokemons())
+        dispatch(tiposPokemon())
+      },[])
     return (
         <div className="landing" > 
             <img  src="https://fontmeme.com/images/Pokemon-Logo.jpg" alt='Pokemon' />

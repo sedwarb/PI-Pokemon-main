58cf4bc5bd6f582641aabdbbf78e347c860cd55c
sedwarb <sedwarb@gmail.com>
2022-04-20 13:19:43 -0500

    add new componet PokemonPorNombre is a input type text to search pokemon by name

---
+++ client/src/components/PokemonPorNombre/PokemonPorNombre.jsx
@@ -0,0 +1,23 @@
+import React,{useState} from 'react';
+import { useDispatch,useSelector } from 'react-redux'
+import {getPokemonByName} from '../../actions/actions'
+
+
+export default function PokemonPorNombre(){
+    const [pokeName, setPokeName] = useState({});
+    const pokemons = useSelector(state=>state.pokemonsLoaded)
+    const dispatch = useDispatch()
+    function handleChange(e){
+        setPokeName({[e.target.id]: e.target.value})
+    }
+    return <div>
+        <form onSubmit={(e) => {                        
+            e.preventDefault();
+            dispatch(getPokemonByName(pokeName.buscar,pokemons))
+            }}>
+            <span className='buSpan'>ByName...</span>
+            <input type='search' id="buscar" onChange={(e) => handleChange(e)} />
+            <input type="submit" value={`Buscar`} />
+        </form>
+    </div>
+}

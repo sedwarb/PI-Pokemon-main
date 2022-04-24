ebf85a458db7503f0dddec39c2a0ddfafdbfb7f8
sedwarb <sedwarb@gmail.com>
2022-04-24 12:04:12 -0500

    add redux state sTipo to update pokemon Types selected

--- client/src/components/ListaTipos/ListaTipos.jsx
+++ client/src/components/ListaTipos/ListaTipos.jsx
@@ -1,17 +1,17 @@
 import React,{useState,useEffect} from 'react';
-import {tiposList} from '../../actions/actions'
-import { useDispatch } from 'react-redux'
+import {tiposList,sTpos} from '../../actions/actions'
+import { useDispatch, useSelector } from 'react-redux'
 
 
 export default function ListTypes ({lTipos}) {
-    const [sTipo, setSTipo] = useState([]);
     const [datos, setDatos] = useState({acum:true});
+    const sTipo = useSelector(state=>state.sTipo)
     const dispatch = useDispatch()
     function ftipo(e){
         if(datos.acum){
             setDatos({acum: false})
         }else{
-          setSTipo([...sTipo,{[e.target.id]:e.target.value}])
+          dispatch(sTpos([...sTipo,{[e.target.id]:e.target.value}]))
           setDatos({type:sTipo})
           setDatos({acum: true})
         }        
@@ -27,8 +27,8 @@ export default function ListTypes ({lTipos}) {
                 return <option key={`plista${i}`} value={`${tipo.id}:${tipo.name}`} id={tipo.id} >{tipo.name}</option>
             })}
         </select>
-        <div className='divTipo' key={`divTipo`} >{sTipo.map(p=><div>{`- ${p.tipos.split(":")[1]} -`}</div>)}
-        <input type="button" key={`butlim`} value="Limpiar" onClick={()=>setSTipo([])} />
+        <div className='divTipo' key={`divTipo`} id="divTipo" >{sTipo.map(p=><div>{`- ${p.tipos.split(":")[1]} -`}</div>)}
+        <input type="button" key={`butlim`} value="Limpiar" onClick={()=>dispatch(sTpos([]))} />
         </div>
       </div>
     );

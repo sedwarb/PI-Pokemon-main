6f253cb0cd1390ec64c0fa9e99f41f2eae4cf6cc
sedwarb <sedwarb@gmail.com>
2022-04-24 14:18:10 -0500

    before it charge pokemons dont show the boton to inicio

--- client/src/components/landing/landing.jsx
+++ client/src/components/landing/landing.jsx
@@ -1,18 +1,37 @@
 import React,{useEffect} from 'react';
-import { useDispatch } from 'react-redux'
-import { NavLink } from 'react-router-dom';
+import { useDispatch,useSelector } from 'react-redux'
+import { Link } from 'react-router-dom';
 import {getPokemons,tiposPokemon} from '../../actions/actions'
+import './landing.css'
 
 export default function Landing() {
+    const pokemons = useSelector(state=>state.orden)
     const dispatch = useDispatch()
+    const img = "https://25.media.tumblr.com/a1e87d2030a73aee16661e8807da6c1d/tumblr_mkhnmmFwaA1rxvkeso1_500.gif"
+    const imgBola = "https://reygif.com/media/4/pokebola-54401.gif"
+    
     useEffect(()=>{        
         dispatch(getPokemons())
         dispatch(tiposPokemon())
-      },[])
-    return (
-        <div className="landing" > 
-            <img  src="https://fontmeme.com/images/Pokemon-Logo.jpg" alt='Pokemon' />
-            <h3><NavLink exact to="/pokemons" >Inicio</NavLink></h3>            
-        </div>
-    )
+      },[pokemons])
+    
+    if(pokemons.length>0){
+        return (
+            <div className="landing" >                
+                <div className='divBotonn'><button className='botonDDiv'><Link to="/pokemons" >Inicio</Link></button></div>
+                <br />
+                <div className='divimagen'><img className='imagen' src={img} alt='Pokemon' /></div>
+            </div>
+        )
+    }else{
+        return (
+            <div className="landing" >
+                <div className='divBotonn'><span className='spanBotonn'>Cargado... Pokemons Por favor Espere</span></div>
+                <br />
+                <div className='divimagen'><img className='imagen' src={img} alt='Pokemon' /></div>
+                <br />
+                <div className='divBola'><img className='imagenBola' src={imgBola} alt='Pokebola' /></div>
+            </div>
+        )
+    }
 }

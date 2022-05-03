88979eac01916f249b56f154616742870103bee4
sedwarb <sedwarb@gmail.com>
2022-05-02 22:41:30 -0500

    change in css of componet pokecard

--- client/src/components/PokemonCard/PokemonCard.jsx
+++ client/src/components/PokemonCard/PokemonCard.jsx
@@ -13,12 +13,12 @@ export default function PokemonCard({pokemon,indice,detalle}) {
         }
         </div>
     }else{
-        return <div key={`pokeCb${pokemon.nombre}`}>
+        return <div key={`pokeCb${pokemon.nombre}`} className="allPokeCard">
         {
             <div key={`pokeb${pokemon.nombre}`}>
                 <img className='ipoke' src={pokemon.imagen} alt='Pokemon' key={`imgpokeb${pokemon.nombre}`}/>
-                <div key={`pokeh1b${pokemon.nombre}`}><Link key={`linkb${indice}`} to="/pokemons/detail" >{pokemon.nombre}</Link></div>                  
-                <div key={`poke2b${pokemon.nombre}`}>{pokemon.tipos.map((t,j)=><div key={`pokeh2b${pokemon.nombre}${j}`}>{t}</div>)}</div>                  
+                <div className="PokeCardName" key={`pokeh1b${pokemon.nombre}`}><Link key={`linkb${indice}`} to="/pokemons/detail" >{pokemon.nombre}</Link></div>                  
+                <div className="PokeCardtype" key={`poke2b${pokemon.nombre}`}>{pokemon.tipos.map((t,j)=><div className="PokeCardtypeA" key={`pokeh2b${pokemon.nombre}${j}`}>{t}</div>)}</div>                  
             </div>
         }
         </div>
